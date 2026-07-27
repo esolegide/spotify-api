5acf240fee46bb101e522ab6ed42b6a48a980ef3
megide <esolegide@gmail.com>
2026-07-27 12:18:02 +0100

    fix: correct formatting of load_dotenv() in settings.py

--- spotify/settings.py
+++ spotify/settings.py
@@ -15,7 +15,8 @@ import os
 from dotenv import load_dotenv
 import dj_database_url
 
-load_dotenv()       2   w# Build paths inside the project like this: BASE_DIR / 'subdir'.
+load_dotenv()
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
