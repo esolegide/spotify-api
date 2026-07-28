5436f671303ba924c0567e68de940a427f47d6da
megide <esolegide@gmail.com>
2026-07-28 12:39:06 +0100

    fix: add templates directory path and static root in settings.py

--- spotify/settings.py
+++ spotify/settings.py
@@ -66,7 +66,9 @@ ROOT_URLCONF = "spotify.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            os.path.join(BASE_DIR, "templates"),
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -138,6 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
